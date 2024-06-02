38c8bdcddc1875ca4ab5e318ceb61ffe8374b25a
abdesslambeka <hamadoukhadidja2@gmail.com>
2024-06-02 18:52:09 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('تم إرسال رسالتك بنجاح!');
+});
+
+// يمكن إضافة المزيد من التأثيرات التفاعلية هنا إذا لزم الأمر
